61090eda48cc5c20657a0b8dbc30af6c778fc673
Sabin Gautam <sabingautam@Sabins-MacBook-Pro.local>
2022-11-18 04:39:17 +0545

    first

---
+++ src/test/java/com/learn/kafkka/learnkafka/LearnkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.kafkka.learnkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
